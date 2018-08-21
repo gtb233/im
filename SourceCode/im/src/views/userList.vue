1d0f9110261a9a514d2a008303472e0e3faf2233
gtb <gdluck256@gmail.com>
2018-08-21 14:58:08 +0800

    userlist view

---
+++ SourceCode/im/src/views/userList.vue
@@ -0,0 +1,19 @@
+<template>
+			<div class="socket-box">
+          <div class="socket-left">
+            待补充
+          </div>
+          <div class="socket-right">
+            待补充
+          </div>
+			</div>
+</template>
+
+<script>
+export default {
+    name:"Index",
+    components:{
+    }
+}
+</script>
+
